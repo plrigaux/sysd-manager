0ce99bc0314432a34ae286550c051d3bb60130b9
Pier <491172+plrigaux@users.noreply.github.com>
2024-07-12 23:38:39 -0400

    scrit for flathub

--- coprtest.py
+++
@@ -1,9 +0,0 @@
-from copr.v3 import Client
-from pprint import pprint
-client = Client.create_from_config_file()
-pprint(client.config)
-
-
-from copr.v3 import config_from_file
-config = config_from_file()
-client = Client(config)
