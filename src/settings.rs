15fa2b98bbe0d3e0db51b7696fd7d88071218789
plrigaux <491172+plrigaux@users.noreply.github.com>
2024-07-22 00:06:34 -0400

    get the stetting from gtk api

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,36 @@
-use crate::systemd;
+use crate::gtk::prelude::SettingsExt;
+use gtk::gio;
 use log::info;
 use log::warn;
 
 pub fn set_color_scheme(settings: &gtk::Settings) {
-    let mut cmd = systemd::commander(&[
+    const SCHEMA: &str = "org.gnome.desktop.interface";
+    const KEY: &str = "color-scheme";
+
+    let Some(settings_schema_source) = gio::SettingsSchemaSource::default() else {
+        warn!("Can't intanciate SettingsSchemaSource");
+        return;
+    };
+
+    let Some(setting_schema) = settings_schema_source.lookup(SCHEMA, true) else {
+        warn!("Schema '{}' not found", SCHEMA);
+        return;
+    };
+
+    if !setting_schema.has_key(KEY) {
+        warn!("Key '{}' not found on schema '{}'", KEY, SCHEMA);
+    }
+
+    let gio_settings = gio::Settings::new(SCHEMA);
+
+    let color_scheme_value = gio_settings.value(KEY);
+
+    let Some(color_scheme) = color_scheme_value.str() else {
+        warn!("Key '{}' not a string", KEY);
+        return;
+    };
+
+    /*     let mut cmd = systemd::commander(&[
         "gsettings",
         "get",
         "org.gnome.desktop.interface",
@@ -27,13 +54,17 @@ pub fn set_color_scheme(settings: &gtk::Settings) {
             warn!("Error parsing color-scheme: {:?}", e);
             return;
         }
-    };
+    }; */
 
-    info!("color-scheme value: \"{}\"", color_scheme_value);
+    set_dark_theme(color_scheme, settings);
+}
 
-    if color_scheme_value.contains("prefer-dark") {
+fn set_dark_theme(color_scheme: &str, settings: &gtk::Settings) {
+    info!("color-scheme value: '{}'", color_scheme);
+
+    if color_scheme.contains("prefer-dark") {
         set_application_prefer_dark_theme(settings, true);
-    } else if color_scheme_value.contains("default") {
+    } else if color_scheme.contains("default") {
         set_application_prefer_dark_theme(settings, false);
     }
 }
