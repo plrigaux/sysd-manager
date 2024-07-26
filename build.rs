33d2ed44b9db659fc4060f18f322106b38e13ad0
plrigaux <491172+plrigaux@users.noreply.github.com>
2024-07-26 11:16:31 -0400

    Cargo compile automatically schema

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ fn main() {
         "data/resources/resources.gresource.xml",
         "sysd-manager.gresource",
     );
+
+    compile_schema();
 }
 
 // BELOW CODE is COPY of glib-build-tools = "0.19.0"
@@ -11,7 +13,11 @@ fn main() {
 
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use std::{env, path::Path, process::Command};
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 // rustdoc-stripper-ignore-next
 /// Call to run `glib-compile-resources` to generate compiled gresources to embed
@@ -69,5 +75,66 @@ pub fn compile_resources<P: AsRef<Path>>(source_dirs: &[P], gresource: &str, tar
     let output = String::from_utf8(output).unwrap();
     for dep in output.split_whitespace() {
         println!("cargo:rerun-if-changed={dep}");
+    }
+}
+
+fn compile_schema() {
+    const GLIB_SCHEMAS_DIR: &str = ".local/share/glib-2.0/schemas/";
+    const GLIB_SCHEMAS_FILE: &str = "data/schemas/io.github.plrigaux.sysd-manager.gschema.xml";
+
+    let path = Path::new(GLIB_SCHEMAS_FILE);
+    println!("Path {:?}", path);
+    let schema_file = match fs::canonicalize(path) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("Error: {:?}", e);
+            return;
+        }
+    };
+
+    let home_dir = env::var("HOME").unwrap();
+
+    let out_dir = PathBuf::from(home_dir).join(GLIB_SCHEMAS_DIR);
+
+    println!("print out_dir {:?}", out_dir);
+
+    println!("cargo:rerun-if-changed={GLIB_SCHEMAS_FILE}");
+    let mut command = Command::new("install");
+    let output = command
+        .arg("-v")
+        .arg("-D")
+        .arg(schema_file)
+        .arg("-t")
+        .arg(&out_dir)
+        .output()
+        .unwrap();
+
+    println!("Install Schema");
+    println!(
+        "Install Schema stdout {}",
+        String::from_utf8_lossy(&output.stdout)
+    );
+    println!(
+        "Install Schema stderr {}",
+        String::from_utf8_lossy(&output.stderr)
+    );
+    println!("Install Schema status {}", output.status);
+
+    let mut command = Command::new("glib-compile-schemas");
+    let output = command.arg(&out_dir).output().unwrap();
+
+    if output.status.success() {
+        println!("Compile Schema Done on '{:?}'", out_dir);
+    } else {
+        println!("Compile Schema Failed on '{:?}'", out_dir);
+        println!(
+            "Compile Schema stdout {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        println!(
+            "Compile Schema stderr {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+        println!("Compile Schema status {}", output.status);
     }
 }
