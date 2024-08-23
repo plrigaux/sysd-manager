cfbc57be8519a71efe82af8edfb416ce86e47595
plrigaux <491172+plrigaux@users.noreply.github.com>
2024-08-23 10:37:21 -0400

    Works with param for status

--- src/widget/info_window/mod.rs
+++ src/widget/info_window/mod.rs
@@ -83,58 +83,6 @@ impl InfoWindow {
         obj
     }
 
-    /*     fn setup_settings(&self) {
-           let settings = gio::Settings::new(systemd_gui::APP_ID);
-           self.imp()
-               .settings
-               .set(settings)
-               .expect("`settings` should not be set before calling `setup_settings`.");
-       }
-
-       fn settings(&self) -> &gio::Settings {
-           self.imp()
-               .settings
-               .get()
-               .expect("`settings` should be set in `setup_settings`.")
-       }
-
-       pub fn save_window_size(&self) -> Result<(), glib::BoolError> {
-           // Get the size of the window
-           let size = self.default_size();
-
-           // Set the window state in `settings`
-           self.settings().set_int("window-width", size.0)?;
-           self.settings().set_int("window-height", size.1)?;
-           self.settings()
-               .set_boolean("is-maximized", self.is_maximized())?;
-
-           Ok(())
-       }
-
-       fn load_window_size(&self) {
-           // Get the window state from `settings`
-           let mut width = self.settings().int("window-width");
-           let mut height = self.settings().int("window-height");
-           let is_maximized = self.settings().boolean("is-maximized");
-
-           info!("Window settings: width {width}, height {height}, is-maximized {is_maximized}");
-
-           if width < 0 {
-               width = 1280;
-           }
-
-           if height < 0 {
-               height = 720;
-           }
-           // Set the size of the window
-           self.set_default_size(width, height);
-
-           // If the window was maximized when it was closed, maximize it again
-           if is_maximized {
-               self.maximize();
-           }
-       }
-    */
     fn load_dark_mode(&self) {}
 
     pub fn fill_data(&self, unit: &UnitInfo) {
@@ -155,5 +103,9 @@ impl InfoWindow {
         } else {
             warn!("Store not supposed to be None");
         };
+
+        let mut title = String::from("Unit Info - ");
+        title.push_str(&unit.primary());
+        self.set_title(Some(&title));
     }
 }
--- src/widget/service_info/imp.rs
+++ src/widget/service_info/imp.rs
@@ -1,5 +1,6 @@
 use gtk::{glib, prelude::*, subclass::prelude::*};
-use log::error;
+use log::{error, warn};
+use zvariant::{DynamicType, Value};
 
 use crate::{
     systemd::{self, data::UnitInfo},
@@ -77,36 +78,121 @@ impl ServiceStatusImp {
         self.name_description
             .set_label(&format!("{} - {}", unit.primary(), unit.description()));
 
-        let map = match systemd::fetch_system_unit_info(&unit) {
+        let map = match systemd::fetch_system_unit_info_native(&unit) {
             Ok(m) => m,
             Err(e) => {
                 error!("Fail to retreive Unit info: {:?}", e);
                 return;
             }
         };
-        
-        if let Some(load_state) = map.get("LoadState") {
-            self.info_loaded
-                .set_label(load_state);
+
+        if let Some(value) = map.get("DropInPaths") {
+            let drop_in_paths = match value as &zvariant::Value {
+                zvariant::Value::Array(a) => {
+                    let mut vec = Vec::with_capacity(a.len());
+
+                    let mut it = a.iter();
+                    while let Some(mi) = it.next() {
+                        vec.push(value_str(mi));
+                    }
+
+                    vec
+                }
+                _ => {
+                    warn!("Wrong zvalue conversion: {:?}", value.dynamic_signature());
+                    vec![]
+                }
+            };
+
+            self.info_loaded.set_label(&drop_in_paths.join("\n"));
         }
 
         if let Some(active_state) = map.get("ActiveState") {
-            self.info_active
-                .set_label(active_state);
+            self.info_active.set_label(value_str(active_state));
         }
 
-        if let Some(drop_in_paths) = map.get("DropInPaths") {
-            self.info_dropin
-                .set_label(drop_in_paths);
+        if let Some(load_state) = map.get("LoadState") {
+            self.info_dropin.set_label(value_str(load_state));
         }
 
-        if let Some(main_pid) = map.get("MainPID") {
-            self.info_mainpid
-                .set_label(&format!("{main_pid} ({})", unit.display_name()));
+        if let Some(value) = map.get("MainPID") {
+            if let zvariant::Value::U32(main_pid) = value as &Value {
+                if 0 == *main_pid {
+                    self.info_mainpid.set_label(" - ");
+                } else {
+                    self.info_mainpid
+                        .set_label(&format!("{} ({})", main_pid, unit.display_name()));
+                }
+            }
+        }
+
+        if let Some(value) = map.get("MemoryCurrent") {
+            let memory_current = match value as &zvariant::Value {
+                zvariant::Value::U64(v) => v,
+                _ => {
+                    warn!("Wrong zvalue conversion: {:?}", value.dynamic_signature());
+                    &0
+                }
+            };
+
+            let mem = if *memory_current == 18446744073709551615_u64 {
+                ""
+            } else {
+                &human_bytes(*memory_current)
+            };
+
+            self.info_memory.set_label(mem);
         }
     }
+
+    //TODO Documentation
 }
 
 impl ObjectImpl for ServiceStatusImp {}
 impl WidgetImpl for ServiceStatusImp {}
 impl GridImpl for ServiceStatusImp {}
+
+const SUFFIX: [&str; 9] = ["B", "K", "M", "G", "T", "P", "E", "Z", "Y"];
+const UNIT: f64 = 1024.0;
+
+fn value_str<'a>(value: &'a Value<'a>) -> &'a str {
+    if let zvariant::Value::Str(converted) = value as &Value {
+        return converted.as_str();
+    }
+    warn!("Wrong zvalue conversion: {:?}", value);
+    //String::new()
+    ""
+}
+
+/// Converts bytes to human-readable values
+fn human_bytes(bytes: u64) -> String {
+    // let size: f64 = *bytes as f64;
+
+    if bytes <= 0 {
+        return "0 B".to_string();
+    }
+
+    let base = (bytes as f64).log10() / UNIT.log10();
+
+    let mut result: String = format!("{:.1}", UNIT.powf(base - base.floor()))
+        .trim_end_matches(".0")
+        .to_string();
+
+    result.push_str(" ");
+    result.push_str(SUFFIX[base.floor() as usize]);
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::widget::service_info::imp::human_bytes;
+    #[test]
+    fn test1() {
+        println!("{}", human_bytes(0));
+        println!("{}", human_bytes(3));
+        println!("{}", human_bytes(18446744073709551615));
+        println!("{}", human_bytes(1024));
+    }
+}
