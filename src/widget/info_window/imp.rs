2a6ca2b2f0059cc87a321f19c319eb01272460cd
plrigaux <491172+plrigaux@users.noreply.github.com>
2024-08-23 12:07:57 -0400

    Unit properties in clipboard

--- src/widget/info_window/imp.rs
+++ src/widget/info_window/imp.rs
@@ -1,6 +1,9 @@
+use gtk::prelude::*;
 use gtk::subclass::prelude::*;
 use gtk::{gio, glib};
 use std::cell::RefCell;
+
+use crate::info::rowitem;
 
 // ANCHOR: imp
 #[derive(Debug, Default, gtk::CompositeTemplate)]
@@ -13,6 +16,33 @@ pub struct InfoWindowImp {
     pub(super) store: RefCell<Option<gio::ListStore>>,
 }
 
+#[gtk::template_callbacks]
+impl InfoWindowImp {
+    #[template_callback]
+    fn handle_copy_click(&self, _button: &gtk::Button) {
+        let clipboard = _button.clipboard();
+
+        let unit_prop_store = &self.store;
+        //unit_prop_store.borrow()
+        if let Some(store) = unit_prop_store.borrow().as_ref() {
+            let n_item = store.n_items();
+
+            let mut data = String::new();
+            for i in 0..n_item {
+                if let Some(object) = store.item(i) {
+                    if let Ok(x) = object.downcast::<rowitem::Metadata>() {
+                        data.push_str(&x.col1());
+                        data.push('\t');
+                        data.push_str(&x.col2());
+                        data.push('\n')
+                    }
+                }
+            }
+            clipboard.set_text(&data)
+        }
+    }
+}
+
 #[glib::object_subclass]
 impl ObjectSubclass for InfoWindowImp {
     const NAME: &'static str = "InfoWindow";
@@ -20,8 +50,8 @@ impl ObjectSubclass for InfoWindowImp {
     type ParentType = gtk::Window;
 
     fn class_init(klass: &mut Self::Class) {
-        // The layout manager determines how child widgets are laid out.
         klass.bind_template();
+        klass.bind_template_callbacks();
     }
 
     fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
