ac8770e3441259a4db6bfa6fdf01401a67006709
plrigaux <491172+plrigaux@users.noreply.github.com>
2025-11-05 15:09:02 -0500

    journal not editable

--- src/widget/journal/imp.rs
+++ src/widget/journal/imp.rs
@@ -683,7 +683,7 @@ impl JournalPanelImp {
 
     fn new_text_view(&self) {
         info!("new_text_view");
-        let tv: gtk::TextView = gtk::TextView::builder().build();
+        let tv: gtk::TextView = gtk::TextView::builder().editable(false).build();
         self.scrolled_window.set_child(Some(&tv));
         self.journal_text_view.replace(tv);
         self.time_old_new.set(None);
