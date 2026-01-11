c0942fdc3b09d5d7e0ac0fbf962d4dc9dd4b2f25
plrigaux <491172+plrigaux@users.noreply.github.com>
2026-01-10 19:24:47 -0500

    rem install script

--- packaging/aur/packaging.py
+++ packaging/aur/packaging.py
@@ -1,14 +1,15 @@
-import build_aux.build_common as bc
-from  build_aux.build_common import color
-import os
-import re
 import argparse
+import os
 
-AUR_DIR="../aur/sysd-manager"
-PKGBUILD="PKGBUILD"
-INSTALL_FILE="sysd-manager.install" 
-AUR_OUT_DIR="../aur/sysd-manager"
-TEMPLATE_DIR='packaging/aur'
+import build_aux.build_common as bc
+from build_aux.build_common import color
+
+AUR_DIR = "../aur/sysd-manager"
+PKGBUILD = "PKGBUILD"
+# INSTALL_FILE="sysd-manager.install"
+AUR_OUT_DIR = "../aur/sysd-manager"
+TEMPLATE_DIR = "packaging/aur"
+
 
 def main():
     os.chdir("..")
@@ -28,18 +29,18 @@ def main():
             "make",
             "genpush",
             "check",
-            "install"
+            "install",
         ],
-        help="action to perform",)
-    
+        help="action to perform",
+    )
 
-    parser.add_argument("-r", "--release", help="Set the package release",  type=int)
+    parser.add_argument("-r", "--release", help="Set the package release", type=int)
 
     args = parser.parse_args()
 
-    release=None
+    release = None
     if args.release:
-        release=args.release
+        release = args.release
 
     match args.action:
         case "generate":
@@ -61,117 +62,118 @@ def main():
 
 
 def create_pkgbuild(release=None):
-    #version
+    # version
     version = bc.get_version()
     print(f"Version {color.BOLD}{color.CYAN}{version}{color.END}")
 
-    #set commit tag
+    # set commit tag
     tag_name = bc.get_version_tag()
     print(f"Tag name {color.BOLD}{color.CYAN}{tag_name}{color.END}")
-
 
     commit = bc.get_tag_commit(tag_name)
 
     print(f"Commit {color.BOLD}{color.CYAN}{commit}{color.END}")
 
-
     pkgbuild_text = ""
 
-    with open(f'{TEMPLATE_DIR}/{PKGBUILD}', "r") as pkgbuild_file:
+    with open(f"{TEMPLATE_DIR}/{PKGBUILD}", "r") as pkgbuild_file:
         pkgbuild_text = pkgbuild_file.read()
 
-    #set the version
+    # set the version
     pkgbuild_text = pkgbuild_text.replace("pkgver=\n", f"pkgver={version}\n")
 
-    #set the version
-    
-    if isinstance(release, int):        
+    # set the version
+
+    if isinstance(release, int):
         pkgbuild_text = pkgbuild_text.replace("pkgrel=1\n", f"pkgrel={release}\n")
 
-    #put the commit label
+    # put the commit label
     pkgbuild_text = pkgbuild_text.replace("_commit=\n", f"_commit={commit}\n")
 
+    with open(f"{AUR_OUT_DIR}/{PKGBUILD}", "w") as pkgbuild_file:
+        pkgbuild_file.write(pkgbuild_text)
 
-    with open(f'{AUR_OUT_DIR}/{PKGBUILD}', "w") as pkgbuild_file:
-        pkgbuild_file.write(pkgbuild_text)        
-    
-    
-    #print(pkgbuild_text)
-    #sums
+    # print(pkgbuild_text)
+    # sums
 
-    bc.cmd_run(["cp", f"{TEMPLATE_DIR}/{INSTALL_FILE}", f"{AUR_OUT_DIR}"])
-    bc.cmd_run(["cp", f"CHANGELOG.md", f"{AUR_OUT_DIR}"])
+    # bc.cmd_run(["cp", f"{TEMPLATE_DIR}/{INSTALL_FILE}", f"{AUR_OUT_DIR}"])
+    bc.cmd_run(["cp", "CHANGELOG.md", f"{AUR_OUT_DIR}"])
 
 
 def do_check_sum():
-    cmd = ["makepkg", "-g"]  
+    cmd = ["makepkg", "-g"]
     checksum = bc.cmd_run_str(cmd, cwd=f"{AUR_OUT_DIR}")
 
-    #checksum = checksum.replace("'","\"") 
+    # checksum = checksum.replace("'","\"")
     print("OUT: ", checksum)
 
     pkgbuild_text = ""
 
-    with open(f'{AUR_OUT_DIR}/{PKGBUILD}', "r") as pkgbuild_file:
+    with open(f"{AUR_OUT_DIR}/{PKGBUILD}", "r") as pkgbuild_file:
         print("WRITE SUM on ")
         pkgbuild_text = pkgbuild_file.read()
-        #set the version
-    
+        # set the version
+
     pkgbuild_text = pkgbuild_text.replace("sha256sums=()\n", checksum)
 
-    with open(f'{AUR_OUT_DIR}/{PKGBUILD}', "w") as pkgbuild_file:
+    with open(f"{AUR_OUT_DIR}/{PKGBUILD}", "w") as pkgbuild_file:
         print("WRITE SUM ")
 
-        pkgbuild_file.write(pkgbuild_text)      
-         
+        pkgbuild_file.write(pkgbuild_text)
+
+
 def check_package():
-    cmd = ["namcap", "--info", "PKGBUILD"]  
+    cmd = ["namcap", "--info", "PKGBUILD"]
     bc.cmd_run_str(cmd, cwd=f"{AUR_OUT_DIR}")
 
+
 def generate_sourceinfo():
-    cmd = ["makepkg", "--printsrcinfo"]  
+    cmd = ["makepkg", "--printsrcinfo"]
     printsrcinfo = bc.cmd_run_str(cmd, cwd=f"{AUR_OUT_DIR}")
 
-    with open(f'{AUR_OUT_DIR}/.SRCINFO', "w") as srcinfo_file:
+    with open(f"{AUR_OUT_DIR}/.SRCINFO", "w") as srcinfo_file:
         print("WRITE .SRCINFO")
-        srcinfo_file.write(printsrcinfo)     
+        srcinfo_file.write(printsrcinfo)
+
 
 def install():
-    cmd = ["makepkg", "--install"]  
+    cmd = ["makepkg", "--install"]
     printsrcinfo = bc.cmd_run_str(cmd, cwd=f"{AUR_OUT_DIR}")
-    
-    with open(f'{AUR_OUT_DIR}/.SRCINFO', "w") as srcinfo_file:
+
+    with open(f"{AUR_OUT_DIR}/.SRCINFO", "w") as srcinfo_file:
         print("WRITE .SRCINFO")
-        srcinfo_file.write(printsrcinfo) 
+        srcinfo_file.write(printsrcinfo)
 
 
-def gen_pkfile(release=None): 
+def gen_pkfile(release=None):
     create_pkgbuild(release)
     do_check_sum()
     generate_sourceinfo()
 
 
-def generate_and_push(release=None): 
+def generate_and_push(release=None):
     gen_pkfile(release)
 
     push()
+
 
 def push():
     tag_name = bc.get_version_tag()
 
     print(f"Commit {color.BOLD}{color.CYAN}{tag_name}{color.END}")
 
-    bc.cmd_run(["git", "commit", "-a", "-m", f"\"{tag_name}\""], cwd=f"{AUR_OUT_DIR}")
+    bc.cmd_run(["git", "commit", "-a", "-m", f'"{tag_name}"'], cwd=f"{AUR_OUT_DIR}")
 
     print(f"{color.BOLD}{color.CYAN}Push on AUR{color.END}")
 
     bc.cmd_run(["git", "push"], cwd=f"{AUR_OUT_DIR}")
 
 
-def make(release=None): 
+def make(release=None):
     gen_pkfile(release)
 
     bc.cmd_run(["makepkg"], cwd=f"{AUR_OUT_DIR}")
+
 
 def clean():
     list_dir = [
@@ -180,6 +182,5 @@ def clean():
 
     for f in list_dir:
         print(f"{color.BOLD}Deleting{color.END} {color.YELLOW}{f}{color.END}")
-        #x = " ".join(["rm", "-fr", f])
+        # x = " ".join(["rm", "-fr", f])
         bc.cmd_run(["rm", "-fr", f], cwd=f"{AUR_OUT_DIR}", shell=True)
-
