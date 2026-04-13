4062be147b1c3c2f2fbd096f074bca927d2bcccf
plrigaux <491172+plrigaux@users.noreply.github.com>
2026-04-13 16:24:20 -0400

    tokio runtime

--- sysd-manager-comcontroler/src/lib.rs
+++ sysd-manager-comcontroler/src/lib.rs
@@ -166,7 +166,12 @@ impl CompleteUnitPropertiesCallParams {
 
 pub fn runtime() -> &'static Runtime {
     static RUNTIME: OnceLock<Runtime> = OnceLock::new();
-    RUNTIME.get_or_init(|| Runtime::new().expect("Setting up tokio runtime needs to succeed."))
+    RUNTIME.get_or_init(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .expect("Setting up tokio runtime needs to succeed.")
+    })
 }
 
 ///Try to Start Proxy
