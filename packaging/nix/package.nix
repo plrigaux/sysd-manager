b3db27bb2713b60710f6b8fdb874cc0a426612a5
plrigaux <491172+plrigaux@users.noreply.github.com>
2026-08-14 09:23:18 -0400

    workable nix package

--- packaging/nix/package.nix
+++ packaging/nix/package.nix
@@ -1,5 +1,5 @@
-{ lib,
-  pkgs,
+{
+  lib,
   rustPlatform,
   pkg-config,
   gtk4,
@@ -9,25 +9,28 @@
   gettext,
   glib,
   gsettings-desktop-schemas,
+  fetchFromGitHub,
+  wrapGAppsHook4,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "sysd-manager";
-  version = "2.20.9";
+  version = "2.20.10";
+  __structuredAttrs = true;
 
   src = fetchFromGitHub {
     owner = "plrigaux";
     repo = "sysd-manager";
-    tag = "v${finalAttrs.version}"; #or commit
-    hash = "sha256-GYMLV4hffaIbqUp1b5ERo2QQqiKRlHe9oXfq+wNH/hM=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-FkqWQ66QdUCo9eXtfwxP5IRB5elZn1F0GtED67fvnyA=";
   };
-  
-  #cargoHash = "sha256-CsmcXlkOec/KJ59Ng7MyGsfjWQ80YyV6MztRFULmvDA=";
-  cargoLock.lockFile = ./Cargo.lock;
 
+  cargoHash = "sha256-EJ64ZGPmQM+Gs5O4WkUMJmT8zDRyF4ZjzXGi1Jw+ovA=";
 
   nativeBuildInputs = [
     pkg-config
+    glib
+    wrapGAppsHook4
   ];
 
   buildInputs = [
@@ -40,51 +43,64 @@ rustPlatform.buildRustPackage (finalAttrs: {
     gsettings-desktop-schemas
   ];
 
-  doCheck = false;
+  doCheck = true;
+
+  nativeCheckInputs = [ systemd ];
 
   cargoBuildFlags = [
-    "--release"
     "--features"
     "default"
+    "--package"
+    "sysd-manager"
+    "--package"
+    "sysd-manager-proxy"
   ];
 
   postBuild = ''
     cargo run -p transtools -- packfiles
+    cargo run -p transtools -- mo
   '';
 
-  postInstall = ''
-    install -Dm755 target/release/sysd-manager "$out/bin/sysd-manager"
-    #install -Dm755 target/release/sysd-manager-proxy "$out/bin/sysd-manager-proxy"
+  postInstall =
+    let
+      conf_file = "$out/share/dbus-1/system.d/io.github.plrigaux.SysDManager.conf";
+      service_file = "$out/lib/systemd/system/sysd-manager-proxy.service";
+      bus_name = "io.github.plrigaux.SysDManager";
+    in
+    ''
 
-    install -Dm644 data/icons/hicolor/scalable/apps/io.github.plrigaux.sysd-manager.svg \
-      "$out/share/icons/hicolor/scalable/apps/io.github.plrigaux.sysd-manager.svg"
+      install -Dm644 data/icons/hicolor/scalable/apps/io.github.plrigaux.sysd-manager.svg  "$out/share/icons/hicolor/scalable/apps/io.github.plrigaux.sysd-manager.svg"
+      install -Dm644 target/loc/io.github.plrigaux.sysd-manager.desktop $out/share/applications/io.github.plrigaux.sysd-manager.desktop
+      install -Dm644 target/loc/io.github.plrigaux.sysd-manager.metainfo.xml $out/share/metainfo/io.github.plrigaux.sysd-manager.metainfo.xml
+      install -Dm644 data/schemas/io.github.plrigaux.sysd-manager.gschema.xml -t $out/share/gsettings-schemas/$name/glib-2.0/schemas
+      glib-compile-schemas $out/share/gsettings-schemas/$name/glib-2.0/schemas/
 
-    install -Dm644 data/schemas/io.github.plrigaux.sysd-manager.gschema.xml \
-      "$out/share/glib-2.0/schemas/io.github.plrigaux.sysd-manager.gschema.xml"
+      install -Dm644 sysd-manager-proxy/data/io.github.plrigaux.SysDManager.conf "${conf_file}"
+      install -Dm644 target/loc/io.github.plrigaux.SysDManager.policy $out/share/polkit-1/actions/io.github.plrigaux.SysDManager.policy
+      install -Dm644 sysd-manager-proxy/data/50-io.github.plrigaux.SysDManager.rules $out/share/polkit-1/rules.d/50-io.github.plrigaux.SysDManager.rules
+      install -Dm644 sysd-manager-proxy/data/sysd-manager-proxy.service "${service_file}"
 
-    install -Dm644 target/loc/io.github.plrigaux.sysd-manager.desktop \
-      "$out/share/applications/io.github.plrigaux.sysd-manager.desktop"
-    install -Dm644 target/loc/io.github.plrigaux.sysd-manager.metainfo.xml \
-      "$out/share/metainfo/io.github.plrigaux.sysd-manager.metainfo.xml"
 
-    cp -r target/locale "$out/share/"
+      substituteInPlace "${conf_file}" \
+        --replace-fail '{BUS_NAME}' "${bus_name}" \
+        --replace-fail '{DESTINATION}' "${bus_name}" \
+        --replace-fail '{INTERFACE}' "${bus_name}"
 
-    #install -Dm644 sysd-manager-proxy/data/io.github.plrigaux.SysDManager.conf \
-    #  "$out/share/dbus-1/system.d/io.github.plrigaux.SysDManager.conf"
-    #install -Dm644 target/loc/io.github.plrigaux.SysDManager.policy \
-    #  "$out/share/polkit-1/actions/io.github.plrigaux.SysDManager.policy"
-    #install -Dm644 sysd-manager-proxy/data/50-io.github.plrigaux.SysDManager.rules \
-    #  "$out/share/polkit-1/rules.d/50-io.github.plrigaux.SysDManager.rules"
-    #install -Dm644 sysd-manager-proxy/data/sysd-manager-proxy.service \
-    #  "$out/lib/systemd/system/sysd-manager-proxy.service"
-  '';
+      substituteInPlace "${service_file}" \
+        --replace-fail '{ENVIRONMENT}' "" \
+        --replace-fail '{EXECUTABLE}' "$out/bin/sysd-manager-proxy" \
+        --replace-fail '{DESTINATION}' "${bus_name}" \
+        --replace-fail '{SERVICE_ID}' 'sysd-manager-proxy'
 
-  meta = with lib; {
+      cp -r "./target/locale" "$out/share/"
+    '';
+
+  meta = {
     description = "A systemd GUI to manage service, timer, socket and other units.";
     homepage = "https://github.com/plrigaux/sysd-manager";
-    license = licenses.gpl3Plus;
-    maintainers = [ plrigaux ];
-    platforms = platforms.linux;
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ plrigaux ];
+    platforms = lib.platforms.linux;
     mainProgram = "sysd-manager";
   };
-}
+})
